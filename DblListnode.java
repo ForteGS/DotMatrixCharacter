c538787a8dabe1bc43a6d472b62781927eabbdab
Minh Bui <minh.forte@gmail.com>
2014-02-23 21:45:38 -0600

    implement DBL&MsgLoop

--- DblListnode.java
+++ DblListnode.java
@@ -1,4 +1,85 @@
 
 public class DblListnode<E> {
-
+	
+	private DblListnode<E> prev;
+	private DblListnode<E> next;
+	private E data;
+	
+	/**
+	 * Constructor with full parameters.
+	 * @param prev
+	 * @param next
+	 * @param data
+	 */
+	public DblListnode(DblListnode<E> prev, DblListnode<E> next, E data)	{
+		this.prev = prev;
+		this.next = next;
+		this.data = data;
+	}
+	
+	/**
+	 * Constructor with no parameters.
+	 */
+	public DblListnode()	{
+		this(null, null, null);
+	}
+	
+	/**
+	 * Constructor with only data provided.
+	 * @param data
+	 */
+	public DblListnode(E data)	{
+		this(null, null, data);
+	}
+	
+	/**
+	 * Access the data in this node.
+	 * @return data 
+	 */
+	public E getData()	{
+		return this.data;
+	}
+	
+	/**
+	 * Get the next node of this node.
+	 * @return the next node.
+	 */
+	public DblListnode<E> getNext()	{
+		return next;
+	}
+	
+	/**
+	 * Get the previous node of this node.
+	 * @return
+	 */
+	public DblListnode<E> getPrev()	{
+		return prev;
+	}
+	
+	/**
+	 * Change the data in this node.
+	 * @param data
+	 */
+	public void setData(E data){
+		this.data = data;
+	}
+	
+	/**
+	 * Change the address of the next node of this node.
+	 * @param next
+	 */
+	public void setNext(DblListnode<E> next)	{
+		this.next = next;
+	}
+	
+	/**
+	 * Change the address of the previous node of this node.
+	 * @param prev
+	 */
+	public void setPrev(DblListnode<E> prev)	{
+		this.prev = prev;
+	}
+	
+	
+		
 }
--- MessageLoop.java
+++ MessageLoop.java
@@ -3,37 +3,69 @@ import java.util.Iterator;
 
 public class MessageLoop<E> implements LoopADT<E> {
 	private DblListnode<E> msgLoop;
+	private int numItems;
+	private DblListnode<E> curr;
 	
 	public MessageLoop()	{
 		msgLoop = new DblListnode<E>();
+		numItems = 0;
+	}
+	
+	public void add(E item)	{
+		msgLoop.setData(item);
+		msgLoop.setNext(msgLoop);
+		msgLoop.setPrev(msgLoop);
+		numItems++;
+		curr = msgLoop;
 	}
 	
 	public void addBefore(E item)	{
+		if (numItems == 0)	
+			add(item);
 		
+		DblListnode<E> newItem = new DblListnode<E>(curr.getPrev(), curr, item);
+		curr.getPrev().setNext(newItem);
+		curr.setPrev(newItem);
+		curr = newItem;
 	}
 	
 	public void addAfter(E item)	{
+		if (numItems == 0)	
+			add(item);
 		
+		DblListnode<E> newItem = new DblListnode<E>(curr, curr.getNext(), item);
+		curr.getNext().setPrev(newItem);
+		curr.setNext(newItem);
+		curr = newItem;
 	}
 	
 	public E getCurrent()	{
-		
+		return curr.getData();
 	}
 	
 	public E removeCurrent()	{
+		if (curr == null || numItems == 0)
+			throw new EmptyLoopException();
+		if (numItems == 1)
+			msgLoop = new DblListnode<E>();
 		
+		E tmp = curr.getData();
+		curr.getPrev().setNext(curr.getNext());
+		curr.getNext().setPrev(curr.getPrev());
+		curr = curr.getNext();
+		return tmp;
 	}
 	
 	public void forward()	{
-		
+		curr = curr.getNext();
 	}
 	
 	public void back()	{
-		
+		curr = curr.getPrev();
 	}
 	
 	public int size()	{
-		return 0;
+		return numItems;
 	}
 	
 	public Iterator<E> iterator()	{
