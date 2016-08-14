cc09e1f9a62940cb6883269e97a6d738b58156bd
Minh <minh.forte@gmail.com>
2016-08-13 20:38:54 -0700

    Update DblListnode.java

--- DblListnode.java
+++ DblListnode.java
@@ -1,29 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//                   ALL STUDENTS COMPLETE THESE SECTIONS
-// Main Class File:  Dot Matrix Character 
-// File:             DblListnode.java
-// Semester:         CS367 Spring 2014
-//
-// Author:           Minh Bui
-// CS Login:         minh
-// Lecturer's Name:  Jim Skrentny
-// Lab Section:      null
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
-//                   CHECK ASSIGNMENT PAGE TO see IF PAIR-PROGRAMMING IS ALLOWED
-//                   If allowed, learn what PAIR-PROGRAMMING IS, 
-//                   choose a partner wisely, and complete this section.
-//
-// Pair Partner:     null
-// CS Login:         null
-// Lecturer's Name:  null
-// Lab Section:      null
-//
-//                   STUDENTS WHO GET HELP FROM ANYONE OTHER THAN THEIR PARTNER
-// Credits:          null
-//////////////////////////// 80 columns wide //////////////////////////////////
-
-
 /**
  * This class implements the doubly linked list data structure.
  * @author Minh Bui
